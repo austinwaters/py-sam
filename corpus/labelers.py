fc2fe8ac327fbcfafa92879306d4ff81ee5d9bde
Austin Waters <austin@horatio-245.cs.utexas.edu>
2012-01-16 22:58:15 -0600

    Add 13scene labeler

--- corpus/labelers.py
+++ corpus/labelers.py
@@ -2,6 +2,9 @@
 Defines labelers for evidence files, i.e. functions that map the document names to their class labels.
 """
 
+import os
+
+
 def twenty_news(name):
     # E.g. 20news-bydate-train/alt.atheism/51154 => alt.athiesm
     parts = name.split('/')
@@ -10,7 +13,13 @@ def twenty_news(name):
     return parts[1]
 
 
+def thirteen_scene(name):
+    # Ex. /foo/bar/bedroom/image_0001.jpg.png => bedroom
+    return os.path.basename(os.path.dirname(os.path.abspath(name)))
+
+
 # Register labelers here so they can be referenced by name on the command line
 registry = {
-    '20news':twenty_news
+    '20news':twenty_news,
+    '13scene':thirteen_scene,
 }
