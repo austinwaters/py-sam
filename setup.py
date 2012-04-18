317e4737d369e46cccba81bbab04b04c2093dfa4
Bryan Silverthorn <bcs@cargo-cult.org>
2012-04-18 17:37:35 -0500

    Make a PyPI-able setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@ import setuptools
 import setuptools.extension
 
 setuptools.setup(
-    name = "sam",
+    name = "py-sam",
     version = "0.1",
     packages = setuptools.find_packages(),
     install_requires = [
         #"Cython>=0.15.1",
         "numpy>=1.6.1",
         "scipy>=0.10.0",
+        ],
+    author = "Austin Waters",
+    author_email = "austin@cs.utexas.edu",
+    description = "the spherical admixture topic model",
+    license = "MIT",
+    keywords = "topic models machine learning",
+    url = "http://www.cs.utexas.edu/~austin/",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2.6",
+        "Operating System :: Unix",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         ],
     )
 
